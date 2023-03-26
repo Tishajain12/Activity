e491ca9970c97aa82a0fde5fbeca4782a23da69b
Tishajain12 <126847902+Tishajain12@users.noreply.github.com>
2023-03-26 10:03:01 +0000

    main

---
+++ Conversion.py
@@ -0,0 +1,13 @@
+'''Conversion: Input some numbers, do some simple arithmetic to do silly conversions'''
+
+
+#conversion of degree celcius into fahrenheit
+n=float(input("enter temperature in degree celcius: "))
+C=float(9/5*n+32)
+print(C)
+
+
+#conversion of seconds into minutes
+n=int(input("enter seconds: "))
+m=n/60
+print(m)
